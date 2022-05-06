ea8c43d09192c87962de19e270c2fe93ea879170
PA <pierreadrien.vasseur@gmail.com>
2022-05-06 17:16:36 +0200

    :tada: Import boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# BotBackOfficeBack
+# API
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm i -g nodemon
+npm run dev
+```
+
+## Notes
+
+Boilerplate from https://github.com/nexus9111/personal_api_boilerplate
---
+++ server.js
@@ -0,0 +1,45 @@
+const { port, env } = require('./conf/vars');
+
+const moment = require('moment');
+
+const app = require("./conf/express");
+const mongo = require("./conf/mongo");
+const logger = require("./conf/logger");
+
+const routerUtils = require("./utils/routerUtils");
+
+const auth = require("./router/authRoute");
+const file = require("./router/fileRoute");
+
+mongo.connect();
+
+let BLACKLIST =["178.20.55.18"];
+
+app.all('*', function (req, res, next) {
+    let ipAddress = req.ipInfo.ip.substr(0, 7) == "::ffff:" ? req.ipInfo.ip.substr(7) : req.ipInfo.ip;
+    if (BLACKLIST.indexOf(ipAddress) === -1) {
+        if (!routerUtils.isAuthorizedRoute(req)) {
+            res.status(404).json({
+                "success": false,
+                "data": { "message": "Route does not exist" }
+            });
+        } else {
+            console.log(`🔵 ${ipAddress} called route ${req.originalUrl} (🕦 ${moment().format('l')} ${moment().format('LTS')})`);
+            next();
+        }
+    } else {    
+        res.status(403).json({
+            "success": false,
+            "data": { "message": ipAddress + " IP is not in whiteList" }
+        });
+    }
+});
+
+app.use("/users", auth);
+app.use("/files", file);
+
+app.listen(port, () => {
+    console.log(`🤖 Application running in: ${env}`)
+    console.log(`🛡  Blacklisted ip(s): ${BLACKLIST}`);
+    console.log(`✅ Example app listening at http://localhost:${process.env.PORT_DEV}`);
+});
